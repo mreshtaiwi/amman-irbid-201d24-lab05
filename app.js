8c4fc2507f403dc7e0905e462297bafa0d9d5dc3
Maram Abu Murad <maramabumurad97@gmail.com>
2021-04-29 23:06:02 +0300

    solving4

--- app.js
+++ app.js
@@ -78,12 +78,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; 
 
 function sumArray(sumArr) {
- 
+ let sum1 = sum(testArray[0],testArray[1])[0];
+ let sum2 = sum( sum1, testArray[2])[0];
+  return [ sum2 , "2,3,4 was passed in as an array of numbers, and 9 is their sum." ]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
